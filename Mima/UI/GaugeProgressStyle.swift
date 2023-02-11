930664b0f4b5d20a066fe3c5c39be198340dabbe
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-02-11 13:15:17 +0000

    Progress style tweak

--- Mima/UI/GaugeProgressStyle.swift
+++ Mima/UI/GaugeProgressStyle.swift
@@ -1,14 +1,22 @@
 import SwiftUI
 
 struct GaugeProgressStyle: ProgressViewStyle {
+    @State private var spin = false
+
     func makeBody(configuration: Configuration) -> some View {
         let fractionCompleted = configuration.fractionCompleted ?? 0
 
         ZStack {
             if fractionCompleted == 0 {
                 Circle()
-                    .stroke(.tint.opacity(0.3), style: StrokeStyle(lineWidth: 3.5, lineCap: .butt))
-                    .rotationEffect(.degrees(-90))
+                    .trim(from: 0, to: 0.75)
+                    .stroke(.tint.opacity(0.2), style: StrokeStyle(lineWidth: 3.5, lineCap: .butt))
+                    .rotationEffect(.degrees(spin ? 360 : 0))
+                    .onAppear {
+                        withAnimation(.linear(duration: 1).repeatForever(autoreverses: false)) {
+                            spin.toggle()
+                        }
+                    }
             }
 
             Circle()
